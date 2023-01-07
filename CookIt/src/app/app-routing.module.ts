c63e275d62fab9b4a3c4080e82599811e8ea3936
Nikola Velikov <niki.velikov09@gmail.com>
2023-01-07 11:51:46 +0200

    routing
    Signed-off-by: Nikola Velikov <niki.velikov09@gmail.com>

--- CookIt/src/app/app-routing.module.ts
+++ CookIt/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './auth/login-form/login-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginFormComponent,
+  //canActivate:[NotloggedGuard] ToDo: not logged guard
+
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
