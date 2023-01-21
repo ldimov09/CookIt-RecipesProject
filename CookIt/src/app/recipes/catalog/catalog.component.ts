d5babb2f1750956e8068f4fa1f050eade77c4c00
Nikola Velikov <niki.velikov09@gmail.com>
2023-01-21 18:13:42 +0200

    Connection between the catalog and the server
    Signed-off-by: Nikola Velikov <niki.velikov09@gmail.com>

--- CookIt/src/app/recipes/catalog/catalog.component.ts
+++ CookIt/src/app/recipes/catalog/catalog.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.scss']
+  styleUrls: ['./catalog.component.scss'],
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+   recipes!:any
+   arr:any=[]
+  constructor(private recipeSurvice: RecipeService, router: Router) {}
+  ngOnInit() {
+    this.recipeSurvice.getAllRecipes().subscribe({
+    next:(value:any)=>{
+      this.recipes= value.result;
+      for (const recipe of this.recipes) {
+        for (let i = 1; i <= recipe.servings; i++) {
+          this.arr.push(i)
+         
+        }
+        recipe.servings=this.arr
+        this.arr=[]
+      }
+console.log(this.recipes);
 
+    }
+   })
+   
+  }
 }
