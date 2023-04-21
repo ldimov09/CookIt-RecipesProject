7a456d0ffee5da36a3e02608c37c74e30d5cac48
Nikola Velikov <niki.velikov09@gmail.com>
2023-04-22 00:19:40 +0300

    adding the new video
    Signed-off-by: Nikola Velikov <niki.velikov09@gmail.com>

--- CookIt/src/app/video-urls.ts
+++ CookIt/src/app/video-urls.ts
@@ -1,3 +1,4 @@
 export const videoUrls: any = {
   '56': 'https://www.youtube.com/watch?v=tSMnVX2aVWk&ab_channel=Cookit',
+  '57': 'https://www.youtube.com/watch?v=5HSYFSmDq6U&ab_channel=Cookit',
 };
